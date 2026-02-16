ad5e3d8cbeb6dabe3b34e5c90c8d0d16bb743474
TGudik <144559662+TGudik@users.noreply.github.com>
2026-02-16 12:01:47 +0100

    retter styling

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
                     <p>Jeg er <h2>Tim</h2></p>
                     <h2>Gudiksen</h2>
                 </div>
-                <h4>Jeg studerer til webudvikler på Media College Denmark, og her finder du både noget om mig, og de projekter jeg har lavet i min skole- og fritid</h4>
+                <p className={styles.subTitle}>Jeg studerer til webudvikler på Media College Denmark, og her finder du både noget om mig, og de projekter jeg har lavet i min skole- og fritid</p>
             </div>
         </div>
     )
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -12,7 +12,7 @@ export default function Projects() {
           img={ringhjoernetSnapshot}
           projectTitle="Ringhjørnet"
           projectSubTitle="Webshop projekt med supabase, zustand, stripe og resend"
-          projectDescription="Projektet er bygget i react, og er en fuldt funktionel webshop. Til backend har jeg brugt supabase, hvor jeg har de forskellige datatables: products, categories, profiles, orders og order_items. Med products og categories henter jeg de tilsvarende data, og udskriver dem på siden. Hvis brugeren er logget ind som en admin kan man tilgå ruterne /admin/products, hvor man kan oprette produkter, og /admin/orders, hvor man kan se en oversigt over ordrerne der har været på siden. Produkter kan gemmes ned i en cart, til det har jeg brugt zustand, som også gør at jeg globalt kan tilgå produkterne i kurven. Betaling foregår med stripe, leveringsoplysninger bliver gemt i orders datatabel, og derefter viderestilles man til stripe checkout siden, som håndterer betalingen, gemmer ordreren i backend, sender en bekræftelsesmail og sender brugeren tilbage til en success-side. Login-oplysninger for at se adminsider er E-mail: testadmin@test.dk Password: admin123.
+          projectDescription="Projektet er bygget i react, og er en fuldt funktionel webshop. Til backend bruges supabase, hvor data gemmes i de forskellige datatables: products, categories, profiles, orders og order_items. Med products og categories henter jeg de tilsvarende data, og udskriver dem på siden. Hvis brugeren er logget ind som en admin kan man tilgå ruterne /admin/products, hvor man kan oprette produkter, og /admin/orders, hvor man kan se en oversigt over ordrerne der har været på siden. Produkter kan gemmes ned i en cart, til det bruges zustand, som også gør at man globalt kan tilgå produkterne i kurven. Betaling foregår med stripe, leveringsoplysninger bliver gemt i orders datatabel, og derefter viderestilles til stripe checkout, som håndterer betalingen, gemmer ordreren i backend, sender en bekræftelsesmail og sender brugeren tilbage til en success-side. Login-oplysninger for at se adminsider er E-mail: testadmin@test.dk Password: admin123.
           "
           projectLink="https://www.ringhjoernet.dk"
           githubLink="https://github.com/TGudik/ringhjoernet"
@@ -29,7 +29,7 @@ export default function Projects() {
           img={smukSnapshot}
           projectTitle="Smuk Nu"
           projectSubTitle="Skoleprojekt om webshop med skønhedsprodukter"
-          projectDescription="lorem loremlorem lorem lorem loremloremlorem lorem loremlorem lorem lorem loremloremloremlorem loremlorem lorem lorem loremloremloremlorem loremlorem lorem lorem loremloremloremlorem loremlorem lorem lorem loremloremloremlorem loremlorem lorem lorem loremloremloremlorem loremlorem lorem lorem loremloremloremlorem loremlorem lorem lorem loremloremlorem"
+          projectDescription="Projektet er bygget i react. Siden indeholder data hentet fra et api. Produkterne kan gemmes i kurven, som i navbaren opdatere antal varer i kurven. Forsiden indeholder udvalgte produkter, produkt-siden udskriver alle produkter fra api, QnA siden indeholder spørgsmål med svar der udfoldes når de klikkes og medlemssiden indeholder en form"
           projectLink="https://gudiksentim.github.io/tim-smuk-nu/"
           githubLink="https://github.com/GudiksenTim/tim-smuk-nu"
         />
